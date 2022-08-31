4183b82478ff79c3227c70fbe9637ce572c2ae3b
Filip <ffilip.djokic@gmail.com>
2022-08-31 14:20:55 +0200

    fix in sidebar link-container

--- front-end/inventory/src/components/Sidebar.tsx
+++ front-end/inventory/src/components/Sidebar.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "../sass/main.scss";
 import logo from "../assets/images/logo512.png";
-import user from "../assets/images/user-img.png";
+import userImg from "../assets/images/user-img.png";
 import { routeLinks } from "../assets/links/links";
 import { NavLink, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export const Sidebar = () => {
   const { pathname } = useLocation();
+  // const { user } = useSelector((store: any) => store.user);
 
   return (
     <aside>
@@ -16,8 +18,8 @@ export const Sidebar = () => {
           <h2>My Storage</h2>
         </div>
         <div className="user-profile">
-          <img src={user} alt="logo" />
-          <h4>Jennifer Rass</h4>
+          <img src={userImg} alt="logo" />
+          <h4>{"Jennifer Rass"}</h4>
         </div>
         <div className="route-options">
           {routeLinks.map((item) => {
